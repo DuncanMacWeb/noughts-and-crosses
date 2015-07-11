460d9e707d76e0920dbf5e9f6d4b4984fe238664
Tom Scholl <tom@dustyneuron.com>
2015-07-11 12:40:38 +0100

    more refactoring

--- src/NACViews.js
+++ src/NACViews.js
@@ -25,4 +25,36 @@ export class NACDOMView extends NACView {
 
     this.element.innerHTML = `<table id="nac">${table}</table>`;
   }
+
+  initPlayCallback(playCallback) {
+    this.playCallback = playCallback;
+  }
+
+  initialize(dimensions) {
+    this.dimensions = dimensions;
+
+    this.tdList = document.getElementById('nac')
+      .querySelectorAll('td');
+
+    for (var y = 0; y < 3; y++) {
+      for (var x = 0; x < 3; x++) {
+        this.createOnClick(x, y);
+      }
+    }
+  }
+
+  createOnClick(x, y) {
+    tdList[y*3 + x].addEventListener('click', (event) => {
+      //console.log('tdList[' + (y*3 + x).toString() + ']: y = ' + y.toString() + ', x = ' + x.toString());
+
+      this.playCallback(x, y);
+    });
+  }
+
+  highlightWin(el1, el2, el3) {
+    console.log(this.playersTurn + ' has won');
+    el1.style.background = 'red';
+    el2.style.background = 'red';
+    el3.style.background = 'red';
+  }
 }
--- src/NoughtsAndCrosses.js
+++ src/NoughtsAndCrosses.js
@@ -11,28 +11,33 @@ export class NoughtsAndCrosses {
       this.game[i] = ['', '', '']
     }
 
-    var tdList = document.getElementById('nac')
-      .querySelectorAll('td');
+    this.view.initPlayCallback(this.onPlay);
 
-    for (var y = 0; y < 3; y++) {
-      for (var x = 0; x < 3; x++) {
-        this.createOnClick(y, x);
-      }
-    }
+    this.view.initialize(dimensions);
   }
 
-  highlightWin(el1, el2, el3) {
-    console.log(this.playersTurn + ' has won');
-    el1.style.background = 'red';
-    el2.style.background = 'red';
-    el3.style.background = 'red';
+  onPlay(x, y) {
+    if (!this.gameFinished && this.game[y][x] === '') {
+      this.game[y][x] = this.playersTurn;
+      event.target.innerHTML = this.playersTurn;
+
+      if (this.checkWon()) {
+        this.gameFinished = true;
+      }
+
+      if (this.playersTurn === 'X') {
+        this.playersTurn = 'O';
+      } else {
+        this.playersTurn = 'X';
+      }
+    }
   }
 
   checkWon() {
     // horizontals
     for (var y = 0; y < 3; y++) {
       if (this.game[y][0] !== '' && (this.game[y][0] === this.game[y][1]) && (this.game[y][1] === this.game[y][2])) {
-        highlightWin(
+        this.view.highlightWin(
           tdList[y*3],
           tdList[y*3 + 1],
           tdList[y*3 + 2]);
@@ -43,7 +48,7 @@ export class NoughtsAndCrosses {
     // verticals
     for (var x = 0; x < 3; x++) {
       if (this.game[0][x] !== '' && (this.game[0][x] === this.game[1][x]) && (this.game[1][x] === this.game[2][x])) {
-        highlightWin(
+        this.view.highlightWin(
           tdList[x],
           tdList[1*3 + x],
           tdList[2*3 + x]);
@@ -53,37 +58,16 @@ export class NoughtsAndCrosses {
 
     // diagonals
     if (this.game[0][0] !== '' && (this.game[0][0] === this.game[1][1]) && (this.game[1][1] === this.game[2][2])) {
-      highlightWin(
+      this.view.highlightWin(
         tdList[0], tdList[4], tdList[8]);
       return true;
     }
     if (this.game[0][2] !== '' && (this.game[0][2] === this.game[1][1]) && (this.game[1][1] === this.game[2][0])) {
-      highlightWin(
-      tdList[2], tdList[4], tdList[6]);
+      this.view.highlightWin(
+        tdList[2], tdList[4], tdList[6]);
       return true;
     }
 
     return false;
-  }
-
-  createOnClick(y, x) {
-    tdList[y*3 + x].addEventListener('click', (event) => {
-      //console.log('tdList[' + (y*3 + x).toString() + ']: y = ' + y.toString() + ', x = ' + x.toString());
-
-      if (!this.gameFinished && this.game[y][x] === '') {
-        this.game[y][x] = this.playersTurn;
-        event.target.innerHTML = this.playersTurn;
-
-        if (this.checkWon()) {
-          this.gameFinished = true;
-        }
-
-        if (this.playersTurn === 'X') {
-          this.playersTurn = 'O';
-        } else {
-          this.playersTurn = 'X';
-        }
-      }
-    });
   }
 }
