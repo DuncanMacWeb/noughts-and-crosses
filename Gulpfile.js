96fe4d2f6678c0c3a5dafbb286fa0486f17bd7d0
Tom Scholl <tom@dustyneuron.com>
2015-08-01 20:14:36 +0100

    Add babel polyfill, and include all test files

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,6 +6,7 @@ var gulpJade = require('gulp-jade');
 var babelify = require('babelify');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var glob = require('glob');
 
 jade.filters.babel = jadeBabel({ stage: 0 });
 
@@ -18,31 +19,38 @@ gulp.task('html', function () {
     .pipe(gulp.dest('dist/'))
 });
 
-// from http://advantcomp.com/blog/ES6Modules/
+var browserifyOpts = {
+  debug: true
+};
+var babelOpts = {
+  stage: 0
+};
+
 gulp.task('modules', function() {
-    browserify({
-      entries: './src/index.js',
-      debug: true
-    })
-    .transform(babelify.configure({
-      stage: 0
-    }))
-    .bundle()
-    .pipe(source('index.js'))
-    .pipe(gulp.dest('./dist'));
+  var b = browserify(browserifyOpts);
+  b.add(require.resolve('babel/polyfill'));
+  b.add('./src/index.js');
+
+  b.transform(babelify.configure(babelOpts))
+  .bundle()
+  .pipe(source('index.js'))
+  .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('tests', function() {
-    browserify({
-      entries: './src/test/array.js',
-      debug: true
-    })
-    .transform(babelify.configure({
-      stage: 0
-    }))
-    .bundle()
+gulp.task('tests', function (cb) {
+  glob('./src/test/**/*.js', {}, function (err, files) {
+    var b = browserify(browserifyOpts);
+    b.add(require.resolve('babel/polyfill'));
+    files.forEach(function (file) {
+      console.log('found test file ' + file.toString());
+      b.add(file);
+    });
+    b.transform(babelify.configure(babelOpts));
+    b.bundle()
     .pipe(source('bundle.js'))
     .pipe(gulp.dest('./test'));
+    cb();
+  });
 });
 
 gulp.task('default', ['html', 'modules', 'tests']);
